c7adb583e514c84a1cf55b4f3ab5128cf8a5c25a
Naveen <somalapurinaveen@gmail.com>
2023-12-15 21:37:22 +0530

    data Transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.mlproject import logger
 from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.mlproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -19,6 +20,18 @@ try:
     obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformtion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========X")
 except Exception as e:
     logger.exception(e)
     raise e
---
+++ src/mlproject/components/data_transformation.py
@@ -0,0 +1,33 @@
+import os
+from src.mlproject import logger
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from src.mlproject.entity.config_entity import DataTransformationConfig
+
+
+class DataTransformation:
+    def __init__(self, config: DataTransformationConfig):
+        self.config = config
+
+        ## Note: We can add different data transformation techniques such as Scalar, PCA and all
+        #  and we and perform all kinds of EDA in ML cycle here before passing this data to the model
+
+        # I am only adding train_test_spliting because this data is already cleaned up
+
+    
+    def train_test_spliting(self):
+        data = pd.read_csv(self.config.data_path)
+
+        #split the data into training and test sets. (0.75, 0.25) split.
+        train, test = train_test_split(data)
+
+        train.to_csv(os.path.join(self.config.root_dir, "train.csv"), index = False)
+        test.to_csv(os.path.join(self.config.root_dir, "test.csv"), index = False)
+
+        logger.info("Splited data into training and test sets")
+        logger.info(train.shape)
+        logger.info(test.shape)
+
+        print(train.shape)
+        print(test.shape)
+        
